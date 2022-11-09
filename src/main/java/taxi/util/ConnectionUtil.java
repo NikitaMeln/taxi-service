b09f6946a08930e748405508811c4fb419eaa78b
Nikita Melnikov <bam19921303@gmail.com>
2022-11-09 19:06:49 +0300

    Last commit

--- src/main/java/taxi/util/ConnectionUtil.java
+++ src/main/java/taxi/util/ConnectionUtil.java
@@ -6,8 +6,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/taxi";
-    private static final String USERNAME = "root";
+    private static final String URL = "jdbc:mysql://taxi.cizyodcqfuzr.us-west-2"
+            + ".rds.amazonaws.com/taxi";
+    private static final String USERNAME = "admin";
     private static final String PASSWORD = "19738246";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
